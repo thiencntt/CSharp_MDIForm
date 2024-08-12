d646d657f21afd4f4a096510dd17ca060d8a4766
thiencntt <nguyenchithien@gmail.com>
2024-08-12 19:12:41 +0700

    thêm link

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ namespace CSharp_MDIForm
         public Form1()
         {
             InitializeComponent();
+            // https://learn.microsoft.com/en-us/dotnet/desktop/winforms/advanced/multiple-document-interface-mdi-applications?view=netframeworkdesktop-4.8
         }
     }
 }
